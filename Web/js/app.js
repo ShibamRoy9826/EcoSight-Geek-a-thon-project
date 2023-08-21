c52d9b3d0e1e43f94b34d815391ca4f987cd54cc
Ankush Roy <128731129+AnkushRoy-code@users.noreply.github.com>
2023-08-22 00:27:41 +0530

    Update app.js

--- Web/js/app.js
+++ Web/js/app.js
@@ -1,4 +1,3 @@
-//main nav
 let menuToggle = document.querySelector(".menuToggle");
 let header = document.querySelector("header");
 
@@ -6,3 +5,24 @@ menuToggle.onclick = function () {
   header.classList.toggle("active");
 };
 
+//nav on scroll
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+let navbar = document.getElementById("navbar");
+
+window.addEventListener(
+  "scroll",
+  function handleScroll() {
+    const scrollTopPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTopPosition > lastScrollTop) {
+      console.log("scrolling down");
+      navbar.style.top = "-50px";
+    } else if (scrollTopPosition < lastScrollTop) {
+      console.log("scrolling up");
+      navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
+  },
+  false
+);
