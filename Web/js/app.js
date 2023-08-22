250fb22afa917d2a7da62a5bc4b6edaab0f7119d
Ankush Roy <128731129+AnkushRoy-code@users.noreply.github.com>
2023-08-22 13:28:16 +0530

    Update app.js

--- Web/js/app.js
+++ Web/js/app.js
@@ -14,15 +14,50 @@ window.addEventListener(
   function handleScroll() {
     const scrollTopPosition =
       window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollTopPosition > lastScrollTop) {
-      console.log("scrolling down");
-      navbar.style.top = "-50px";
-    } else if (scrollTopPosition < lastScrollTop) {
-      console.log("scrolling up");
-      navbar.style.top = "0";
+    if (700 < window.innerWidth < 880) {
+      if (scrollTopPosition > lastScrollTop) {
+        navbar.style.top = "-80px";
+      } else if (scrollTopPosition < lastScrollTop) {
+        navbar.style.top = "0";
+      }
+    } else {
+      if (scrollTopPosition > lastScrollTop) {
+        navbar.style.top = "-50px";
+      } else if (scrollTopPosition < lastScrollTop) {
+        navbar.style.top = "0";
+      }
     }
     lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
   },
   false
 );
+
+// loading screen
+var loader = document.getElementById("loader");
+
+function disableEverything() {
+  document.body.style.overflow = "hidden";
+  const allElements = document.querySelectorAll("*");
+
+  allElements.forEach((element) => {
+    element.style.pointerEvents = "none";
+  });
+}
+disableEverything(); //disabling everything
+
+function enableEverything() {
+  document.body.style.overflowY = "auto";
+
+  const allElements = document.querySelectorAll("*");
+
+  allElements.forEach((element) => {
+    element.style.pointerEvents = "auto";
+  });
+}
+
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    loader.style.display = "none";
+    enableEverything(); //enablling everything
+  }, 1000);
+});
